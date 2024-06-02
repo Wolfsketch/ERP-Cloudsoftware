3467b10752a94a1075017d2a0006bef81e4da8e5
Wolfsketch <jason.lommelen@gmail.com>
2024-06-02 19:18:13 +0200

    Admin Authen Google Update / V0.0.1

--- AdminInterface/views.py
+++ AdminInterface/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
-def BASE(request):
+@login_required
+def home(request):
     return render(request, 'base.html')
